b71afd759ea46d09a628f1925a7c24bb8b481975
demirbileku <udemirbilek2@gmail.com>
2021-06-23 16:45:30 +0300

    commit tree c13857e36ab9edce55fceeaaa8e6582be12e7018

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import Signup from "./Signup";
+
+function App () {
+    return (
+        <div>
+            <Navbar />
+            <Switch>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+                <Route exact path="/signup">
+                    <Signup />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                 </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default App
